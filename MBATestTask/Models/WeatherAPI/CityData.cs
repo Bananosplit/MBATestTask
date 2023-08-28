54201a01fd1eac14a2884c0d4cfaf10707cc120a
Bananosplit <Serg-rudk@yandex.ru>
2023-08-28 21:12:53 +0300

    add project with ignore

---
+++ MBATestTask/Models/WeatherAPI/CityData.cs
@@ -0,0 +1,126 @@
+﻿namespace MBATestTask.Models.WeatherAPI
+{
+    public class CityData
+    {
+        public int Version { get; set; }
+        public string Key { get; set; }
+        public string Type { get; set; }
+        public int Rank { get; set; }
+        public string LocalizedName { get; set; }
+        public string EnglishName { get; set; }
+        public string PrimaryPostalCode { get; set; }
+        public Region Region { get; set; }
+        public Country Country { get; set; }
+        public AdministrativeArea AdministrativeArea { get; set; }
+        public TimeZone TimeZone { get; set; }
+        public GeoPosition GeoPosition { get; set; }
+        public bool IsAlias { get; set; }
+        public List<SupplementalAdminArea> SupplementalAdminAreas { get; set; }
+        public List<string> DataSets { get; set; }
+        public Details Details { get; set; }
+    }
+
+    public class AdministrativeArea
+    {
+        public string ID { get; set; }
+        public string LocalizedName { get; set; }
+        public string EnglishName { get; set; }
+        public int Level { get; set; }
+        public string LocalizedType { get; set; }
+        public string EnglishType { get; set; }
+        public string CountryID { get; set; }
+    }
+
+    public class Country
+    {
+        public string ID { get; set; }
+        public string LocalizedName { get; set; }
+        public string EnglishName { get; set; }
+    }
+
+    public class Details
+    {
+        public string Key { get; set; }
+        public string StationCode { get; set; }
+        public double StationGmtOffset { get; set; }
+        public string BandMap { get; set; }
+        public string Climo { get; set; }
+        public string LocalRadar { get; set; }
+        public object MediaRegion { get; set; }
+        public string Metar { get; set; }
+        public string NXMetro { get; set; }
+        public string NXState { get; set; }
+        public int? Population { get; set; }
+        public string PrimaryWarningCountyCode { get; set; }
+        public string PrimaryWarningZoneCode { get; set; }
+        public string Satellite { get; set; }
+        public string Synoptic { get; set; }
+        public string MarineStation { get; set; }
+        public double? MarineStationGMTOffset { get; set; }
+        public string VideoCode { get; set; }
+        public string LocationStem { get; set; }
+        public object PartnerID { get; set; }
+        public List<Source> Sources { get; set; }
+        public string CanonicalPostalCode { get; set; }
+        public string CanonicalLocationKey { get; set; }
+    }
+
+    public class Elevation
+    {
+        public Metric Metric { get; set; }
+        public Imperial Imperial { get; set; }
+    }
+
+    public class GeoPosition
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public Elevation Elevation { get; set; }
+    }
+
+    public class Imperial
+    {
+        public double Value { get; set; }
+        public string Unit { get; set; }
+        public int UnitType { get; set; }
+    }
+
+    public class Metric
+    {
+        public double Value { get; set; }
+        public string Unit { get; set; }
+        public int UnitType { get; set; }
+    }
+
+    public class Region
+    {
+        public string ID { get; set; }
+        public string LocalizedName { get; set; }
+        public string EnglishName { get; set; }
+    }
+
+    public class Source
+    {
+        public string DataType { get; set; }
+        public string source { get; set; }
+        public int SourceId { get; set; }
+        public string PartnerSourceUrl { get; set; }
+    }
+
+    public class SupplementalAdminArea
+    {
+        public int Level { get; set; }
+        public string LocalizedName { get; set; }
+        public string EnglishName { get; set; }
+    }
+
+    public class TimeZone
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double GmtOffset { get; set; }
+        public bool IsDaylightSaving { get; set; }
+        public object NextOffsetChange { get; set; }
+    }
+    
+}
